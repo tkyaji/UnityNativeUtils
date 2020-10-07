4cc0fbd82e8eae3e9ecc36425af4cb88699bebdd
tkyaji <tkyaji@users.noreply.github.com>
2020-10-07 09:41:47 +0900

    Supperted Unity2019.3 or newer

--- Assets/NativeUtils/Editor/NativeUtilsPostProcessBuild.cs
+++ Assets/NativeUtils/Editor/NativeUtilsPostProcessBuild.cs
@@ -14,7 +14,11 @@ public class NativeUtilsPostProcessBuild {
         string projPath = Path.Combine(path, "Unity-iPhone.xcodeproj/project.pbxproj");
         PBXProject proj = new PBXProject();
         proj.ReadFromString(File.ReadAllText(projPath));
-        string target = proj.TargetGuidByName("Unity-iPhone");
+#if UNITY_2019_3_OR_NEWER
+            string target = proj.GetUnityFrameworkTargetGuid();
+#else
+            string target = proj.TargetGuidByName("Unity-iPhone");
+#endif
 
         proj.AddFrameworkToProject(target, "SafariServices.framework", false);
         proj.AddFrameworkToProject(target, "StoreKit.framework", false);
